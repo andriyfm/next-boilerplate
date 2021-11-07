8ffc1ba534b5f1001ea3bf21f78d4f4edf9b5e5d
andriyfm <andrifmsyh@gmail.com>
2021-11-07 19:39:39 +0700

    Setup typescript, eslint, prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "google",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "require-jsdoc": "off",
+  },
+  settings: {
+    react: {
+      version: "latest",
+    },
+  },
+};
