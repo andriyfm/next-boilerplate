ce6428e210bb1d094c03f3a6a4b425c5e4728215
andriyfm <andrifmsyh@gmail.com>
2022-09-16 15:05:59 +0700

    feat: #8 - pwa support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-};
+});
